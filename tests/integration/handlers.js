c35f4666da52759b9a2de991d45c46f0dffaa0b8
Dávid István Bíró <noise64@gmail.com>
2024-06-12 10:28:23 +0200

    rename fetch suite to fetchsync

--- tests/integration/handlers.js
+++ tests/integration/handlers.js
@@ -1,5 +1,5 @@
 export { handler as btoa } from './btoa/btoa.js';
 export { handler as performance } from './performance/performance.js';
 export { handler as crypto } from './crypto/crypto.js';
-export { handler as fetch } from './fetch/fetch.js';
+export { handler as fetchsync } from './fetchsync/fetchsync.js';
 export { handler as timers } from './timers/timers.js';
